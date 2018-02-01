cf4d7f449b3cc381eccb05ab7b48222efcbd1c2c
Taeer Bar-Yam <taeer@necsi.edu>
2018-01-31 19:55:55 -0500

    generate a cylinder upon correct invocation. no normals or UV yet

--- a1/src/MeshGen.java
+++ a1/src/MeshGen.java
@@ -5,31 +5,64 @@ import meshgen.OBJMesh;
 import java.io.IOException;
 class MeshGen {  
   public static void main(String args[]) throws IOException {
-    // Args pArgs = new Args(args);
-    OBJMesh mesh = new OBJMesh();
-    mesh.positions.add(new Vector3(1,0,0));
-    mesh.positions.add(new Vector3(0,1,0));
-    mesh.positions.add(new Vector3(0,0,1));
-    OBJFace face = new OBJFace(3, false, false);
-    face.positions[0] = 0;
-    face.positions[1] = 1;
-    face.positions[2] = 2;
-    mesh.faces.add(face);
-    mesh.writeOBJ("foo.obj");
+    Args pArgs = new Args(args);
+    
+    switch(Args.g){
+      case CYLINDER:
+        cylinder(pArgs.n).writeOBJ(pArgs.o);
+        break;
+      case SPHERE:
+      default:
+        System.exit(-1);
+    }
+  }
+
+  public static OBJMesh cylinder(int n){
+    OBJMesh cylinder = new OBJMesh();
+    cylinder.positions.add(new Vector3(0,  1, 0));
+    cylinder.positions.add(new Vector3(0, -1, 0));
+    for(int i=0;i<n;i++){
+      double rads = (-Math.PI / 2) + (i * 2 * Math.PI / n);
+      float x = (float)Math.cos(rads);
+      float z = (float)Math.sin(rads);
+      cylinder.positions.add(new Vector3(x,  1, z));
+      cylinder.positions.add(new Vector3(x, -1, z));
+      OBJFace topFace = new OBJFace(3, false, false);
+      topFace.positions[2] = 0;
+      topFace.positions[1] = i*2+2;
+      topFace.positions[0] = ((i*2+2) % (2*n))+2;
+      cylinder.faces.add(topFace);
+      OBJFace botFace = new OBJFace(3, false, false);
+      botFace.positions[0] = 1;
+      botFace.positions[1] = i*2+3;
+      botFace.positions[2] = ((i*2+3) % (2*n))+2;
+      cylinder.faces.add(botFace);
+      OBJFace rightFace = new OBJFace(3, false, false);
+      rightFace.positions[0] = i*2+2;
+      rightFace.positions[1] = ((i*2+2) % (2*n))+2;
+      rightFace.positions[2] = ((i*2+3) % (2*n))+2;
+      cylinder.faces.add(rightFace);
+      OBJFace leftFace = new OBJFace(3, false, false);
+      leftFace.positions[2] = i*2+2;
+      leftFace.positions[1] = i*2+3;
+      leftFace.positions[0] = ((i*2+3) % (2*n))+2;
+      cylinder.faces.add(leftFace);
+    }
+    return cylinder;
   }
 }
 
 // class to process and store the command line arguments.
 // modified from a post on piazza
 class Args {
-  public enum MeshType { SPHERE, CYLINDER };
-  MeshType g;   // -g <sphere|cylinder>
-  int n;    // [-n <divisionsU>]
-  int m;    // [-n <divisionsV>]
-  String o; // -o <outfile.obj>
-  String i; // -i <infile.obj>
+  public static enum MeshType { SPHERE, CYLINDER };
+  public static MeshType g;   // -g <sphere|cylinder>
+  public static int n;    // [-n <divisionsU>]
+  public static int m;    // [-n <divisionsV>]
+  public static String o; // -o <outfile.obj>
+  public static String i; // -i <infile.obj>
 
-  Args(String[] args) {
+  public static Args(String[] args) {
     //when undefined
     g = null;
     o = null;
@@ -67,11 +100,11 @@ class Args {
       if( o == null // -o must be specified for either invocation
           || (g == null && i == null) || (g != null && i != null) // -g and -i are separate invocations and cannot both be specified
           || n < 2 || m < 2 ){ // we are promised n and m will be at least 2
-        System.out.println(g);
-        System.out.println(n);
-        System.out.println(m);
-        System.out.println(o);
-        System.out.println(i);
+        // System.out.println(g);
+        // System.out.println(n);
+        // System.out.println(m);
+        // System.out.println(o);
+        // System.out.println(i);
         throw new Exception("invalid arguments");
           }
       //Add tests for correct usage and throw exception if not

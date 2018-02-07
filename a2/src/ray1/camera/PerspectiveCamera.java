d099c8e330928e762f004bf8b57b07364ae2222f
Taeer Bar-Yam <taeer@necsi.edu>
2018-02-07 15:47:59 -0500

    did more thing. persp works

--- a2/src/ray1/camera/PerspectiveCamera.java
+++ a2/src/ray1/camera/PerspectiveCamera.java
@@ -1,5 +1,7 @@
 package ray1.camera;
 
+import egl.math.Vector3;
+import egl.math.Vector3d;
 import ray1.Ray;
 
 /**
@@ -13,44 +15,52 @@ import ray1.Ray;
  */
 public class PerspectiveCamera extends Camera {
 
-    protected float projDistance = 1.0f;
-    public float getProjDistance() { return projDistance; }
-    public void setprojDistance(float projDistance) {
-        this.projDistance = projDistance;
-    }
+  public static Vector3 u;
+  public static Vector3 v;
+  public static Vector3 w;
+  protected float projDistance = 1.0f;
+  public float getProjDistance() { return projDistance; }
+  public void setprojDistance(float projDistance) {
+    this.projDistance = projDistance;
+  }
 
 
-    //TODO#A2: create necessary new variables/objects here, including an orthonormal basis
-    //          formed by three basis vectors and any other helper variables 
-    //          if needed.
+  //TODO#A2: create necessary new variables/objects here, including an orthonormal basis
+  //          formed by three basis vectors and any other helper variables 
+  //          if needed.
 
-    /**
-     * Initialize the derived view variables to prepare for using the camera.
-     */
-    public void init() {
-        // TODO#A2: Fill in this function.
-        // 1) Set the 3 basis vectors in the orthonormal basis,
-        // based on viewDir and viewUp
-        // 2) Set up the helper variables if needed
+  /**
+   * Initialize the derived view variables to prepare for using the camera.
+   */
+  public void init() {
+    w = viewDir.clone().negate().normalize();
+    u = viewUp.clone().cross(w).normalize();
+    v = w.clone().cross(u).normalize();
+    // TODO#A2: Fill in this function.
+    // 1) Set the 3 basis vectors in the orthonormal basis,
+    // based on viewDir and viewUp
+    // 2) Set up the helper variables if needed
 
-    }
+  }
 
-    /**
-     * Set outRay to be a ray from the camera through a point in the image.
-     *
-     * @param outRay The output ray (not normalized)
-     * @param inU The u coord of the image point (range [0,1])
-     * @param inV The v coord of the image point (range [0,1])
-     */
-    public void getRay(Ray outRay, float inU, float inV) {
-        // TODO#A2: Fill in this function.
-        // 1) Transform inU so that it lies between [-viewWidth / 2, +viewWidth / 2] 
-        //    instead of [0, 1]. Similarly, transform inV so that its range is
-        //    [-vieHeight / 2, +viewHeight / 2]
-        // 2) Set the origin field of outRay for a perspective camera.
-        // 3) Set the direction field of outRay for an perspective camera. This
-        //    should depend on your transformed inU and inV and your basis vectors,
-        //    as well as the projection distance.
+  /**
+   * Set outRay to be a ray from the camera through a point in the image.
+   *
+   * @param outRay The output ray (not normalized)
+   * @param inU The u coord of the image point (range [0,1])
+   * @param inV The v coord of the image point (range [0,1])
+   */
+  public void getRay(Ray outRay, float inU, float inV) {
+    outRay.origin.set(viewPoint.clone());
+    outRay.direction.set(w.clone().negate().mul(projDistance).add(u.clone().mul(viewWidth / 2 * (2*inU-1)).add(v.clone().mul(viewHeight / 2 * (2*inV-1)))).normalize());
+    // TODO#A2: Fill in this function.
+    // 1) Transform inU so that it lies between [-viewWidth / 2, +viewWidth / 2] 
+    //    instead of [0, 1]. Similarly, transform inV so that its range is
+    //    [-vieHeight / 2, +viewHeight / 2]
+    // 2) Set the origin field of outRay for a perspective camera.
+    // 3) Set the direction field of outRay for an perspective camera. This
+    //    should depend on your transformed inU and inV and your basis vectors,
+    //    as well as the projection distance.
 
-    }
+  }
 }

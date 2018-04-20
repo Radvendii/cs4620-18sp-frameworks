0ead4fc2f9fc2e6cc88d7671c4e70f2bc2392b56
Taeer Bar-Yam <taeer@necsi.edu>
2018-04-20 16:43:51 -0400

    forgot to commit a5

--- a5/src/splines/BSpline.java
+++ a5/src/splines/BSpline.java
@@ -6,21 +6,44 @@ import egl.math.Vector2;
 
 public class BSpline extends SplineCurve{
 
-	public BSpline(ArrayList<Vector2> controlPoints, boolean isClosed,
-			float epsilon) throws IllegalArgumentException {
-		super(controlPoints, isClosed, epsilon);
-	}
+  public BSpline(ArrayList<Vector2> controlPoints, boolean isClosed,
+      float epsilon) throws IllegalArgumentException {
+    super(controlPoints, isClosed, epsilon);
+  }
 
-	@Override
-	public CubicBezier toBezier(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3,
-			float eps) {
-		//TODO A5
-		
-		return new CubicBezier(new Vector2(), new Vector2(), new Vector2(), new Vector2(), eps);
-		//END SOLUTION
-	}
-	
+  @Override
+  public CubicBezier toBezier(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3,
+      float eps) {
+    // P1 = M1Inverse * M2 *P2
+    Matrix4 bspline = new Matrix4( -1/6f, 1/2f, -1/2f, 1/6f,
+        1/2f, -1f, 1/2f, 0,
+        -1/2f, 0, 1/2f, 0,
+        1/6f, 2/3f, 1/6f, 0);
 
-		
-	
+    Matrix4 bezCoeff = new Matrix4 (-1,  3, -3, 1,
+        3, -6,  3, 0,
+        -3,  3,  0, 0, 
+        1,  0,  0, 0);
+
+    //Just putting this in a Matrix4 because we don't have arbitrary sized matrices
+    Matrix4 bsplinepoints = new Matrix4(p0.x, p0.y, 0, 0,
+        p1.x, p1.y, 0, 0,
+        p2.x, p2.y, 0, 0,
+        p3.x, p3.y, 0, 0);
+
+
+    Matrix4 bezPoints = bezCoeff.invert().mulBefore(bspline.mulBefore(bsplinepoints));
+
+    Vector2 bezPoint0 = new Vector2(bezPoints.m[0], bezPoints.m[4]);
+    Vector2 bezPoint1 = new Vector2(bezPoints.m[1], bezPoints.m[5]);
+    Vector2 bezPoint2 = new Vector2(bezPoints.m[2], bezPoints.m[6]);
+    Vector2 bezPoint3 = new Vector2(bezPoints.m[3], bezPoints.m[7]);
+
+    return new CubicBezier(bezPoint0, bezPoint1, bezPoint2, bezPoint3, eps);
+    //END SOLUTION
+  }
+
+
+
+
 }
--- a5/src/splines/CubicBezier.java
+++ a5/src/splines/CubicBezier.java
@@ -8,114 +8,173 @@ import egl.math.Vector2;
  */
 
 public class CubicBezier {
-	
-	//This Bezier's control points
-	public Vector2 p0, p1, p2, p3;
-	
-	//Control parameter for curve smoothness
-	float epsilon;
-	
-	//The points on the curve represented by this Bezier
-	private ArrayList<Vector2> curvePoints;
-	
-	//The normals associated with curvePoints
-	private ArrayList<Vector2> curveNormals;
-	
-	//The tangent vectors of this bezier
-	private ArrayList<Vector2> curveTangents;
-	
-	
-	/**
-	 * 
-	 * Cubic Bezier Constructor
-	 * 
-	 * Given 2-D BSpline Control Points correctly set self.{p0, p1, p2, p3},
-	 * self.uVals, self.curvePoints, and self.curveNormals
-	 * 
-	 * @param bs0 First Bezier Spline Control Point
-	 * @param bs1 Second Bezier Spline Control Point
-	 * @param bs2 Third Bezier Spline Control Point
-	 * @param bs3 Fourth Bezier Spline Control Point
-	 * @param eps Maximum angle between line segments
-	 */
-	public CubicBezier(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3, float eps) {
-		curvePoints = new ArrayList<Vector2>();
-		curveTangents = new ArrayList<Vector2>();
-		curveNormals = new ArrayList<Vector2>();
-		epsilon = eps;
-		
-		this.p0 = new Vector2(p0);
-		this.p1 = new Vector2(p1);
-		this.p2 = new Vector2(p2);
-		this.p3 = new Vector2(p3);
-		
-		tessellate();
-	}
 
-    /**
-     * Approximate a Bezier segment with a number of vertices, according to an appropriate
-     * smoothness criterion for how many are needed.  The points on the curve are written into the
-     * array self.curvePoints, the tangents into self.curveTangents, and the normals into self.curveNormals.
-     * The final point, p3, is not included, because cubic Beziers will be "strung together".
-     */
-    private void tessellate() {
-    	 // TODO A5
-    	
+  //This Bezier's control points
+  public Vector2 p0, p1, p2, p3;
+
+  //Control parameter for curve smoothness
+  float epsilon;
+
+  //The points on the curve represented by this Bezier
+  private ArrayList<Vector2> curvePoints;
+
+  //The normals associated with curvePoints
+  private ArrayList<Vector2> curveNormals;
+
+  //The tangent vectors of this bezier
+  private ArrayList<Vector2> curveTangents;
+
+
+  /**
+   * 
+   * Cubic Bezier Constructor
+   * 
+   * Given 2-D BSpline Control Points correctly set self.{p0, p1, p2, p3},
+   * self.uVals, self.curvePoints, and self.curveNormals
+   * 
+   * @param bs0 First Bezier Spline Control Point
+   * @param bs1 Second Bezier Spline Control Point
+   * @param bs2 Third Bezier Spline Control Point
+   * @param bs3 Fourth Bezier Spline Control Point
+   * @param eps Maximum angle between line segments
+   */
+  public CubicBezier(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3, float eps) {
+    curvePoints = new ArrayList<Vector2>();
+    curveTangents = new ArrayList<Vector2>();
+    curveNormals = new ArrayList<Vector2>();
+    epsilon = eps;
+
+    this.p0 = new Vector2(p0);
+    this.p1 = new Vector2(p1);
+    this.p2 = new Vector2(p2);
+    this.p3 = new Vector2(p3);
+
+    tessellate(10);
+  }
+
+  /**
+   * 
+   * Cubic Bezier Constructor
+   * 
+   * Given 2-D BSpline Control Points correctly set self.{p0, p1, p2, p3},
+   * self.uVals, self.curvePoints, and self.curveNormals
+   * 
+   * @param bs0 First Bezier Spline Control Point
+   * @param bs1 Second Bezier Spline Control Point
+   * @param bs2 Third Bezier Spline Control Point
+   * @param bs3 Fourth Bezier Spline Control Point
+   * @param eps Maximum angle between line segments
+   * @param rec Recursion depth to go
+   */
+  public CubicBezier(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3, float eps, int rec) {
+    curvePoints = new ArrayList<Vector2>();
+    curveTangents = new ArrayList<Vector2>();
+    curveNormals = new ArrayList<Vector2>();
+    epsilon = eps;
+
+    this.p0 = new Vector2(p0);
+    this.p1 = new Vector2(p1);
+    this.p2 = new Vector2(p2);
+    this.p3 = new Vector2(p3);
+
+    tessellate(rec);
+  }
+
+  /* de Casteljau points */
+  private Vector2 deC(float u, int k, int i){
+    if(k == 0){
+      switch(i){
+        case 0: return p0.clone();
+        case 1: return p1.clone();
+        case 2: return p2.clone();
+        case 3: return p3.clone();
+      }
     }
-	
-    
-    /**
-     * @return The points on this cubic bezier
-     */
-    public ArrayList<Vector2> getPoints() {
-    	ArrayList<Vector2> returnList = new ArrayList<Vector2>();
-    	for(Vector2 p : curvePoints) returnList.add(p.clone());
-    	return returnList;
+    return deC(u, k-1, i).mul(1.0f - u).add(deC(u, k-1, i+1).mul(u));
+  }
+
+  private Vector2 tangentAt(float u){
+    return deC(u,2,1).sub(deC(u,2,0)).mul(3);
+  }
+
+  /**
+   * Approximate a Bezier segment with a number of vertices, according to an appropriate
+   * smoothness criterion for how many are needed.  The points on the curve are written into the
+   * array self.curvePoints, the tangents into self.curveTangents, and the normals into self.curveNormals.
+   * The final point, p3, is not included, because cubic Beziers will be "strung together".
+   */
+  private void tessellate(int rec) {
+    if(p1.clone().sub(p0).angle(p2.clone().sub(p1)) < epsilon && p2.clone().sub(p1).angle(p3.clone().sub(p2)) < epsilon || rec <= 0){
+      curvePoints.add(p0);
+      Vector2 tan = p1.clone().sub(p0).normalize();
+      curveTangents.add(tan);
+      curveNormals.add(tan.clone().cross().cross().cross().normalize());
     }
-    
-    /**
-     * @return The tangents on this cubic bezier
-     */
-    public ArrayList<Vector2> getTangents() {
-    	ArrayList<Vector2> returnList = new ArrayList<Vector2>();
-    	for(Vector2 p : curveTangents) returnList.add(p.clone());
-    	return returnList;
+    else{
+      CubicBezier left = new CubicBezier(p0, deC(0.5f, 1, 0), deC(0.5f, 2, 0), deC(0.5f, 3, 0), epsilon, rec-1);
+      CubicBezier right = new CubicBezier(deC(0.5f, 3, 0), deC(0.5f, 2, 1), deC(0.5f, 1, 2), p3, epsilon, rec-1);
+      curvePoints.addAll(left.getPoints());
+      curvePoints.addAll(right.getPoints());
+      curveTangents.addAll(left.getTangents());
+      curveTangents.addAll(right.getTangents());
+      curveNormals.addAll(left.getNormals());
+      curveNormals.addAll(right.getNormals());
     }
-    
-    /**
-     * @return The normals on this cubic bezier
-     */
-    public ArrayList<Vector2> getNormals() {
-    	ArrayList<Vector2> returnList = new ArrayList<Vector2>();
-    	for(Vector2 p : curveNormals) returnList.add(p.clone());
-    	return returnList;
-    }
-    
-    
-    /**
-     * @return The references to points on this cubic bezier
-     */
-    public ArrayList<Vector2> getPointReferences() {
-    	ArrayList<Vector2> returnList = new ArrayList<Vector2>();
-    	for(Vector2 p : curvePoints) returnList.add(p);
-    	return returnList;
-    }
-    
-    /**
-     * @return The references to tangents on this cubic bezier
-     */
-    public ArrayList<Vector2> getTangentReferences() {
-    	ArrayList<Vector2> returnList = new ArrayList<Vector2>();
-    	for(Vector2 p : curveTangents) returnList.add(p);
-    	return returnList;
-    }
-    
-    /**
-     * @return The references to normals on this cubic bezier
-     */
-    public ArrayList<Vector2> getNormalReferences() {
-    	ArrayList<Vector2> returnList = new ArrayList<Vector2>();
-    	for(Vector2 p : curveNormals) returnList.add(p);
-    	return returnList;
-    }
+  }
+
+
+  /**
+   * @return The points on this cubic bezier
+   */
+  public ArrayList<Vector2> getPoints() {
+    ArrayList<Vector2> returnList = new ArrayList<Vector2>();
+    for(Vector2 p : curvePoints) returnList.add(p.clone());
+    return returnList;
+  }
+
+  /**
+   * @return The tangents on this cubic bezier
+   */
+  public ArrayList<Vector2> getTangents() {
+    ArrayList<Vector2> returnList = new ArrayList<Vector2>();
+    for(Vector2 p : curveTangents) returnList.add(p.clone());
+    return returnList;
+  }
+
+  /**
+   * @return The normals on this cubic bezier
+   */
+  public ArrayList<Vector2> getNormals() {
+    ArrayList<Vector2> returnList = new ArrayList<Vector2>();
+    for(Vector2 p : curveNormals) returnList.add(p.clone());
+    return returnList;
+  }
+
+
+  /**
+   * @return The references to points on this cubic bezier
+   */
+  public ArrayList<Vector2> getPointReferences() {
+    ArrayList<Vector2> returnList = new ArrayList<Vector2>();
+    for(Vector2 p : curvePoints) returnList.add(p);
+    return returnList;
+  }
+
+  /**
+   * @return The references to tangents on this cubic bezier
+   */
+  public ArrayList<Vector2> getTangentReferences() {
+    ArrayList<Vector2> returnList = new ArrayList<Vector2>();
+    for(Vector2 p : curveTangents) returnList.add(p);
+    return returnList;
+  }
+
+  /**
+   * @return The references to normals on this cubic bezier
+   */
+  public ArrayList<Vector2> getNormalReferences() {
+    ArrayList<Vector2> returnList = new ArrayList<Vector2>();
+    for(Vector2 p : curveNormals) returnList.add(p);
+    return returnList;
+  }
 }
